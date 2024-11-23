c345f5773399084c13c71b8bf809b39c910925f9
nainsworth <nja8161995@gmail.com>
2024-11-22 19:15:28 -0600

    Updated Webpack Structure

--- webpack.common.js
+++ webpack.common.js
@@ -30,26 +30,14 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: "development",
   entry: {
-    bundle: path.resolve(__dirname, "src/index.js"),
+    main: path.resolve(__dirname, "src/index.js"),
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
     clean: true,
     assetModuleFilename: "[name][ext]",
-  },
-  devtool: "source-map",
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist/"),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
   },
   module: {
     rules: [
---
+++ webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+
+  devtool: "source-map",
+  devServer: {
+    static: "./dist",
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+});
